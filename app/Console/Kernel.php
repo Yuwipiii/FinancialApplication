e3ff4eec4f909e22ad74ee375252cd5552546ce5
Eldos Ulanbekov <ulanbekoveldos@gmail.com>
2024-02-29 09:25:31 +0600

    создал schedule для получения курса валюты

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->call(GetCurrencyDataCommand::class)->daily();
+        $schedule->call(GetCurrencyDataCommand::class);
     }
 
     /**
