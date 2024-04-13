0c86360b9e8eed30c49e19d3587dbb4c3f7d0fff
Eldos Ulanbekov <ulanbekoveldos@gmail.com>
2024-04-13 23:22:08 +0600

    Создал модель Goal

---
+++ app/Models/Goal.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Goal extends Model
+{
+    use HasFactory;
+    protected $fillable=['user_id','current_amount','target_amount','is_completed','name'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
